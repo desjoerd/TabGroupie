ac454b21bc20307884fe6fa7f455740cfd75cb4e
desjoerd <sjoerd.meer@outlook.com>
2022-11-17 15:38:46 +0100

    remove $schema from manifest when building

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
+delete manifest["$schema"];
+
 export default defineConfig({
   plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
 });
